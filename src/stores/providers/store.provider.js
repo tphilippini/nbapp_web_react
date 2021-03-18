1f1fa582ed8650863a6db70c8929a1b2f66e6c42
tphilippini <thomas.philippini@gmail.com>
2021-03-18 15:46:19 +0100

    Removes Store for test

--- src/stores/providers/store.provider.js
+++ src/stores/providers/store.provider.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 import LocaleProvider from "./locale.provider";
-import LoadingProvider from "./loading.provider";
-import VideoProvider from "./video.provider";
-import Spinner from "../../components/helpers/Spinner.helper";
+// import LoadingProvider from "./loading.provider";
+// import VideoProvider from "./video.provider";
+// import Spinner from "../../components/helpers/Spinner.helpers";
 
 import UserProvider from "./user.provider";
 
 const Store = ({ children }) => {
   return (
     <LocaleProvider>
-      <LoadingProvider>
-        <VideoProvider>
-          <Spinner />
-          <UserProvider>{children}</UserProvider>
-        </VideoProvider>
-      </LoadingProvider>
+      {/* <LoadingProvider> */}
+      {/* <VideoProvider> */}
+      {/* <Spinner /> */}
+      <UserProvider>{children}</UserProvider>
+      {/* </VideoProvider> */}
+      {/* </LoadingProvider> */}
     </LocaleProvider>
   );
 };
